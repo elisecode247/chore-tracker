da1fa1831c99dd4a29da2c309cd578f9de29c085
elisecode247 <elisecode247@gmail.com>
2021-09-15 16:11:20 -0700

    add chores to scheduler

--- client/src/components/layout/events/EventsList.jsx
+++ client/src/components/layout/events/EventsList.jsx
@@ -1,29 +1,54 @@
 import React from 'react';
+import { useGetChoresQuery } from '../../../slices/choresApiSlice';
 import { useGetAllEventsQuery } from '../../../slices/eventsApiSlice';
-import Scheduler from 'devextreme-react/scheduler';
+import Scheduler, { Resource } from 'devextreme-react/scheduler';
 import subMinutes from 'date-fns/subMinutes';
 import addMinutes from 'date-fns/addMinutes';
 import 'devextreme/dist/css/dx.material.purple.light.css';
 import '../../../styles/scheduler.css';
 import agendaStatuses from '../../../constants/agendaStatuses';
 import { useUpdateEventMutation } from '../../../slices/eventsApiSlice';
+import isValid from 'date-fns/isValid';
+import getHours from 'date-fns/getHours';
+import getMinutes from 'date-fns/getMinutes';
+import set from 'date-fns/set';
 
 const eventStatuses = Object.entries(agendaStatuses).map(([value, name]) => ({ name, value }));
-const currentDate = new Date();
+const today = new Date();
 const views = ['day', 'week', 'workWeek', 'month'];
 
 export default function EventsList() {
-
+    const { data: chores } = useGetChoresQuery();
     const { data: events } = useGetAllEventsQuery();
+    const formattedChores = chores && chores.map((chore) => {
+        const scheduledAt = (chore.scheduled_at && new Date(chore.scheduled_at)) || null;
+        const scheduledTime = isValid(scheduledAt) ? { hours: getHours(scheduledAt), minutes: getMinutes(scheduledAt) } :
+            { hours: getHours(today), minutes: getMinutes(today) };
+        const scheduledToday = chore.has_time ? set(today, scheduledTime) : today;
+
+        return {
+            ...chore,
+            text: chore.name,
+            startDate: scheduledToday,
+            endDate: addMinutes(scheduledToday, 30),
+            type: 'chore',
+            allDay: !chore.has_time
+        };
+    });
     const formattedEvents = events && events.map((event) => {
         return {
             ...event,
             text: event.name,
+            type: 'event',
             startDate: !event.started_at ? subMinutes(new Date(event.completed_at), 30) : new Date(event.started_at),
             endDate: !event.completed_at ? addMinutes(new Date(event.started_at), 30) : new Date(event.completed_at),
             allDay: false
         };
     });
+    const allItems = [
+        ...(formattedEvents ? formattedEvents : []),
+        ...(formattedChores ? formattedChores : []),
+    ];
     const [updateEvent] = useUpdateEventMutation();
 
     const handleAppointmentUpdated = function (schedulerEvent) {
@@ -42,16 +67,33 @@ export default function EventsList() {
             <Scheduler id="scheduler"
                 adaptivityEnabled={true}
                 cellDuration={60}
-                dataSource={formattedEvents}
+                dataSource={allItems}
                 views={views}
                 defaultCurrentView="day"
-                defaultCurrentDate={currentDate}
+                defaultCurrentDate={today}
                 startDayHour={6}
                 editing={true}
                 onAppointmentFormOpening={handleAppointmentFormOpening}
                 onAppointmentUpdated={handleAppointmentUpdated}
                 appointmentRender={renderAppointment}
-            />
+            >
+                <Resource
+                    dataSource={[
+                        {
+                            text: 'Chore',
+                            id: 'chore',
+                            color: '#cc5c53'
+                        },
+                        {
+                            text: 'Event',
+                            id: 'event',
+                            color: '#ff9747'
+                        }
+                    ]}
+                    fieldExpr="type"
+                    label="Type"
+                />
+            </Scheduler>
         </div>
     );
 }
